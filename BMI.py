bc2236ccd5d981199a8bff37ce6907c4bb57a0c2
yp0o0 <160223006+yp0o0@users.noreply.github.com>
2024-02-16 18:33:59 +0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -18,16 +18,16 @@ height = st.number_input("Enter your height in Meters", step = 0.01)
 
 def calculate_bmi():
     bmi = weight/(height)**2
-    bmi_thresholds = [18.5, 23, 27.5]
+    bmi_thresholds = [0, 18.5, 23, 27.5]
     level_labels = ['Risk of nutritional deficiency','Low Risk','Moderate Risk','High Risk','Invalid']
     if bmi < bmi_thresholds[0]:
         level = level_labels[4]
         st.error(f'Your BMI is {bmi}. It is {level}')
-    elif bmi <= bmi_thresholds[0]:
-        level = level_labels[0]
     elif bmi <= bmi_thresholds[1]:
-        level = level_labels[1]
+        level = level_labels[0]
     elif bmi <= bmi_thresholds[2]:
+        level = level_labels[1]
+    elif bmi <= bmi_thresholds[3]:
         level = level_labels[2]
     else:
         level = level_labels[3]
